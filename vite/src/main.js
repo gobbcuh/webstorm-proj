a38c0a7f14c648a75b015c4bf459b439361b5da6
jasmin <jajalorino@gmail.com>
2025-06-25 12:14:02 +0800

    added scene to object

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,3 +1,26 @@
 import * as THREE from 'three';
 
 const canvas = document.getElementById('canvas');
+
+// 1. scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshBasicMaterial({color: '#468585'});
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshBasicMaterial({color: '#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry, material);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+
