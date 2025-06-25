7f21bdb106796361596a156327c17d115f4323cd
jasmin <jajalorino@gmail.com>
2025-06-25 12:45:38 +0800

    added animations + emissive properties

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById('canvas');
 
@@ -12,12 +13,12 @@ camera.position.z = 5;
 
 // 3. object
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({color: '#468585'});
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({color: '#B4B4B3'});
-const box = new THREE.Mesh(boxGeometry, material);
+const boxMaterial = new THREE.MeshStandardMaterial({color: '#B4B4B3', emissive: '#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
 scene.add(dodecahedron);
@@ -31,6 +32,30 @@ scene.add(light);
 // 5. renderer
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// 6. orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. animations
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y  += 0.01;
+
+    box.rotation.y += 0.005;
+
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+// 8. window resizing handler
+
+animate();
 
 
